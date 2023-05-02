5e18e555cebbe157b4dade8c86ae43650f6d41bb
dantas404 <130323565+dantas404@users.noreply.github.com>
2023-05-02 12:58:56 -0300

    Update vagrantDependencies.sh

--- Ubuntu/Debian/vagrantDependencies.sh
+++ Ubuntu/Debian/vagrantDependencies.sh
@@ -7,7 +7,10 @@ sudo apt-get install git -y
 
 echo "Installing VirtualBox..."
 
-sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list" && sudo wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - && sudo apt-get update -y && sudo apt-get install virtualbox-5.0 -y
+deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian jammy contrib
+sudo gpg --dearmor oracle_vbox_2016.asc --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+sudo apt-get update -y
+sudo apt-get install virtualbox-6.1 -y
 
 echo "Installing Vagrant..."
 
