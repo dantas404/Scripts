5a194a3778efcedab2008298196cab4517a46f56
dantas404 <dantas.nascimento.701@ufrn.edu.br>
2023-05-02 19:35:54 -0300

    Create CentOS8 files

--- Ubuntu/vagrantDependencies.sh
+++ Ubuntu/vagrantDependencies.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-sudo apt-get update -y
+# You need to add the following lines to your /etc/apt/sources.list file before running this script:
+# deb http://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib
+# deb http://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib
 
 echo "Installing Git..."
-sudo apt-get install git -y
+sudo apt install git -y
 
 echo "Installing VirtualBox..."
 
-deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian jammy contrib
-sudo gpg --dearmor oracle_vbox_2016.asc --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
-sudo apt-get update -y
-sudo apt-get install virtualbox-6.1 -y
+sudo apt install fasttrack-archive-keyring -y
+sudo apt update -y
+sudo apt install virtualbox -y
+sudo apt install virtualbox-ext-pack -y
+
 
 echo "Installing Vagrant..."
 
 sudo wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update -y && sudo apt install vagrant -y
+sudo apt update -y
+sudo apt install vagrant -y
 
 echo "Installing Vagrant plugins..."
 vagrant plugin install vagrant-hostmanager
-vagrant plugin install vagrant-triggers
 
 echo "Clone the Vagrant OpenStack repository..."
 git clone https://github.com/OpenStackCookbook/vagrant-openstack
