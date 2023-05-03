7adaa00b3a8a1befbeaa162599ab4a44794021ad
dantas404 <dantas.nascimento.701@ufrn.edu.br>
2023-05-02 21:13:48 -0300

    modified:   Ubuntu/vagrantDependencies.sh

--- Ubuntu/vagrantDependencies.sh
+++ Ubuntu/vagrantDependencies.sh
@@ -4,16 +4,9 @@
 # deb http://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib
 # deb http://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib
 
-echo "Installing Git..."
-sudo apt install git -y
-
-echo "Installing VirtualBox..."
-
-sudo apt install fasttrack-archive-keyring -y
+echo "Installing dependencies..."
 sudo apt update -y
-sudo apt install virtualbox -y
-sudo apt install virtualbox-ext-pack -y
-
+sudo apt install git fasttrack-archive-keyring gcc perl make kernel-devel virtualbox virtualbox-ext-pack -y
 
 echo "Installing Vagrant..."
 
@@ -21,13 +14,4 @@ sudo wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /us
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update -y
 sudo apt install vagrant -y
-
-echo "Installing Vagrant plugins..."
 vagrant plugin install vagrant-hostmanager
-
-echo "Clone the Vagrant OpenStack repository..."
-git clone https://github.com/OpenStackCookbook/vagrant-openstack
-cd vagrant-openstack
-vagrant up
-
-echo "Done! You can now run 'vagrant ssh' to connect to the VM."
